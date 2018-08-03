dba655c906b62daafc52ddcc6f9b09dc4a8f7d87
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-08-03 16:04:35 +0100

    clean repo and improve analysis

--- examples/analysis_iris.R
+++ examples/analysis_iris.R
@@ -1,8 +1,11 @@
+library(ggplot2)
+library(ggpubr)
+
 df <- read.csv("iris.data", 
                  header = FALSE,
                  sep = ",")
 
-sds <- read.csv("synthetic_iris3.data", 
+sds <- read.csv("synthetic_iris_simple.data", 
                header = FALSE,
                sep = ",")
 
@@ -10,8 +13,33 @@ df$set = rep("observed", nrow(df))
 sds$set = rep("synthetised", nrow(sds))
 all = rbind(df, sds)
 
-library(ggplot2)
-library(ggpubr)
+p1 <- ggplot(all, aes(x=V1, colour=set)) +
+  geom_density() + 
+  facet_wrap(~V5)
+p2 <- ggplot(all, aes(x=V2, colour=set)) +
+  geom_density() + 
+  facet_wrap(~V5)
+p3 <- ggplot(all, aes(x=V3, colour=set)) +
+  geom_density() + 
+  facet_wrap(~V5)
+p4 <- ggplot(all, aes(x=V4, colour=set)) +
+  geom_density() + 
+  facet_wrap(~V5)
+ggarrange(p1, p2, p3, p4)
+
+readline("Press <return to continue")
+
+df <- read.csv("iris.data", 
+               header = FALSE,
+               sep = ",")
+
+sds <- read.csv("synthetic_iris_expon_beta.data", 
+                header = FALSE,
+                sep = ",")
+
+df$set = rep("observed", nrow(df))
+sds$set = rep("synthetised", nrow(sds))
+all = rbind(df, sds)
 
 p1 <- ggplot(all, aes(x=V1, colour=set)) +
   geom_density() + 
@@ -38,3 +66,48 @@ p3 <- ggplot(all, aes(x=V3, colour=set)) +
 p4 <- ggplot(all, aes(x=V4, colour=set)) +
   geom_density()
 ggarrange(p1, p2, p3, p4)
+
+readline("Press <return to continue")
+
+df <- read.csv("iris.data", 
+               header = FALSE,
+               sep = ",")
+
+sds <- read.csv("synthetic_iris_final.data", 
+                header = FALSE,
+                sep = ",")
+
+df$set = rep("observed", nrow(df))
+sds$set = rep("synthetised", nrow(sds))
+all = rbind(df, sds)
+
+p1 <- ggplot(all, aes(x=V1, colour=set)) +
+  geom_density()
+p2 <- ggplot(all, aes(x=V2, colour=set)) +
+  geom_density()
+p3 <- ggplot(all, aes(x=V3, colour=set)) +
+  geom_density()
+p4 <- ggplot(all, aes(x=V4, colour=set)) +
+  geom_density()
+ggarrange(p1, p2, p3, p4)
+
+readline("Press <return to continue")
+
+p1 <- ggplot(all, aes(x=V1, colour=set)) +
+  geom_density() + 
+  facet_wrap(~V5)
+p2 <- ggplot(all, aes(x=V2, colour=set)) +
+  geom_density() + 
+  facet_wrap(~V5)
+p3 <- ggplot(all, aes(x=V3, colour=set)) +
+  geom_density() + 
+  facet_wrap(~V5)
+p4 <- ggplot(all, aes(x=V4, colour=set)) +
+  geom_density() + 
+  facet_wrap(~V5)
+ggarrange(p1, p2, p3, p4)
+
+
+
+
+
