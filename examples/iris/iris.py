96c363eb08179019f267d8d631f7027ac3536c11
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-08-22 16:59:14 +0100

    edit examples but something seems to be broken

--- examples/iris/iris.py
+++ examples/iris/iris.py
@@ -7,7 +7,7 @@ with open("iris.data", "r", newline='') as in_file, open("synthetic_iris4.data",
     ods = [e for e in iris_reader]
 
     metadata = [sdv.FLOAT, sdv.FLOAT, sdv.FLOAT, sdv.FLOAT, sdv.CATEGORICAL]
-    sds = sdv.syn_by_class(metadata, ods, 4, header=header, size=150)
+    sds = sdv.syn(metadata, ods, size=150)
 
     iris_writer = csv.writer(out_file)
     iris_writer.writerows(sds)
